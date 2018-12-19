82f122fb29a3cc348c567c7bdda5e5d5821f150a
Naveen Vedula <naveen@formathletica.com>
2018-12-19 13:45:27 -0800

    adding space

--- finetune.py
+++ finetune.py
@@ -146,6 +146,7 @@ class PrunningFineTuner_VGG16:
             correct += pred.cpu().eq(label).sum()
             total += label.size(0)
 
+
         print "Accuracy :", float(correct) / total
 
         self.model.train()
